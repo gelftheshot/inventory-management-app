effab1cdcc4f3dfdce0075c70954728162ad5910
lihon <gelf.learing@gmail.com>
2024-08-04 08:00:43 +0300

    the better ui

--- components/homepage.jsx
+++ components/homepage.jsx
@@ -1,11 +1,14 @@
+'use client'
 import {Stack} from '@mui/material'
 import Dashboard from './dashboard'
 import Sidebar from './sidebar'
 import { Box } from '@mui/material'
+import Header from './header'
 
 const homepage = () => {
   return (
     <Box>
+      <Header />
         <Stack direction='row' spacing={2}>
         <Box style={{ backgroundColor: '#f0f0f0', width: '350px' }}>
             <Sidebar />
